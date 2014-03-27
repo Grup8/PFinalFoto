4a68f8e81b231b7a36f3b0074cc2a91aac05fd94
jaspreet <jsingh@infobosccoma.net>
2014-03-27 16:27:05 +0100

    GridLayout creat (activity_foto.xml,fragment_foto.xml,FotoActivity.java) : JAS

--- ProjecteMultimediaMuseu/gen/com/example/projectemultimediamuseu/R.java
+++ ProjecteMultimediaMuseu/gen/com/example/projectemultimediamuseu/R.java
@@ -13,6 +13,11 @@ public final class R {
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
@@ -24,18 +29,24 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int GridLayout1=0x7f080001;
+        public static final int action_settings=0x7f080002;
+        public static final int container=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_foto=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int fragment_foto=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int foto=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_foto=0x7f050003;
     }
     public static final class style {
         /** 
