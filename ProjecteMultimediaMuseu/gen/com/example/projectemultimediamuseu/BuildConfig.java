d4d764a51b1184eb3bcb55da46d834620f8c7251
jaspreet <jsingh@infobosccoma.net>
2014-03-27 15:34:32 +0100

    Commit 1 : jaspreet singh

---
+++ ProjecteMultimediaMuseu/gen/com/example/projectemultimediamuseu/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.projectemultimediamuseu;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
